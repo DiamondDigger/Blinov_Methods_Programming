da7802427adc836775fbcd10a822857a2fd92a82
naglec13 <naglec13@gmail.com>
2020-03-17 16:04:10 +0300

    ..String

---
+++ src/SecondChapter/StringExample.java
@@ -0,0 +1,21 @@
+package SecondChapter;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String element = "hello";
+        element += '2';
+        element += "001";
+        element += "/" + 001 + "\n";
+        element = element + 5;
+        element += 3.1233 + "\n" + 353434;
+        System.out.println(element);
+
+        //вывод строки String с помощью литерала
+        String number = "123456789";
+        for (int i =number.length() ; i >0 ; i--) {
+            System.out.println(number.charAt(i-1)+"\n");
+            System.out.println(number= number.replace(number.charAt(i-1), '0'));
+            System.out.println("___________________________");
+        }
+    }
+}
