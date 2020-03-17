be40425ca9064e24bd8cc05ef8a11b33375e107f
naglec13 <naglec13@gmail.com>
2020-03-17 21:42:46 +0300

    ..

--- src/SecondChapter/exercises/findLongestNumber.java
+++ src/SecondChapter/exercises/findLongestNumber.java
@@ -8,21 +8,27 @@ import java.util.Scanner;
 и их длину.
 */
 public class findLongestNumber {
+    private static final String SEQUENCE_OF_NUMBERS = "1 22 333 4 4444 123456789 123 2 12345678 "; // for test
     private static String longerNumber;
+    private static String shorterNumber;
+    private static int minLength = 10;
+    private static int maxLength = 1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputFromConsole = scanner.nextLine();
         System.out.println(inputFromConsole);
         String[] lineOfNumber = inputFromConsole.split(" ");
-        int maxLength = 1;
-        String maxStrNumber = lineOfNumber[0];
         for (String s : lineOfNumber) {
             if (s.length() > maxLength) {
                 maxLength = s.length();
                 longerNumber = s;
+            } else if (s.length() < minLength) {
+                minLength = s.length();
+                shorterNumber = s;
             }
         }
-        System.out.println("longerNumber=" + longerNumber + "; length = " + longerNumber.length());
+        System.out.println("longerNumber= " + longerNumber + "; length = " + longerNumber.length());
+        System.out.println("shorterNumber= " + shorterNumber + "; length = " + shorterNumber.length());
     }
 }
