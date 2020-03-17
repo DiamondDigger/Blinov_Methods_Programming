20d040dd2a84a9fa7a1b4eaf7abdcdfafb876de7
naglec13 <naglec13@gmail.com>
2020-03-17 18:26:59 +0300

    ..

--- src/SecondChapter/DemoArrays.java
+++ src/SecondChapter/DemoArrays.java
@@ -2,8 +2,8 @@ package SecondChapter;
 
 public class DemoArrays {
     public static void main(String[] args) {
-        int ArrInt[] = new int[10];
-        String ArrString[] = new String[4];
+        int[] ArrInt = new int[10];
+        String[] ArrString = new String[4];
         System.out.println("ArrInt[]:");
         for (int elemInt : ArrInt) {
             System.out.print(elemInt+ " - ");
