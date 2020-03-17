daa54779a8ea52e6a1ae60f2ac0c49a10d4a0bab
naglec13 <naglec13@gmail.com>
2020-03-17 13:03:21 +0300

    ..1

---
+++ src/FirstChapter/FirstProgram.java
@@ -0,0 +1,8 @@
+package FirstChapter;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        SloganAction firstObject = new SloganAction();
+        firstObject.writeMessage();
+    }
+}
---
+++ src/FirstChapter/SloganAction.java
@@ -0,0 +1,7 @@
+package FirstChapter;
+
+public class SloganAction {
+    public void writeMessage() {
+        System.out.println("Enabling the information Age");
+    }
+}
