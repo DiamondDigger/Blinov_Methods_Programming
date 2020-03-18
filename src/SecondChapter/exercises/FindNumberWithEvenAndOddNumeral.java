73b21cc4f8f532f3b98cc6610f36297bb103160a
naglec13 <naglec13@gmail.com>
2020-03-19 00:07:32 +0300

    ..not the end

--- src/SecondChapter/exercises/FindNumberWithEvenAndOddNumeral.java
+++ src/SecondChapter/exercises/FindNumberWithEvenAndOddNumeral.java
@@ -1,20 +1,25 @@
 package SecondChapter.exercises;
 
 import java.util.Scanner;
+/*
+Найти количество чисел, содержащих только четные цифры, а среди них —
+количество чисел с равным числом четных и нечетных цифр.
+ */
 
-public class FindNumberWirhEvenAndOddNumeral {
+public class FindNumberWithEvenAndOddNumeral {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputFromConsole = scanner.nextLine();
         String[] strArr = inputFromConsole.split(" ");
         for (String s : strArr) {
             char[] chArr = s.toCharArray();
+            int numberLength = chArr.length;
             for (int i = 0; i < chArr.length; i++) {
                 int number = Character.digit(chArr[i],10);
                 System.out.print(number);
             }
             System.out.print(" ");
-
+            System.out.println(numberLength);
         }
     }
 }
