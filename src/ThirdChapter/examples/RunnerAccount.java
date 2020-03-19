da0a8fd3b92fbf07cad3ed5f8b42af881501ed26
naglec13 <naglec13@gmail.com>
2020-03-19 15:28:16 +0300

    ..

--- src/ThirdChapter/examples/RunnerAccount.java
+++ src/ThirdChapter/examples/RunnerAccount.java
@@ -4,10 +4,32 @@ public class RunnerAccount {
     public static void main(String[] args) {
         Account account1 = new Account();
         Account account2 = new Account(25l);
-        Account account3 = new Account(34,300.0);
+        Account account3 = new Account(34, 300.0);
 
-        System.out.println(account1.createName("Alex")+"\n"+account1.getId()+"\n"+account1.getAmount()+"\n"+"__________________");
-        System.out.println(account1.createName("Liana")+"\n"+account1.getId()+"\n"+account1.getAmount()+"\n"+"__________________");
-        System.out.println(account1.createName("John")+"\n"+account1.getId()+"\n"+account1.getAmount()+"\n"+"__________________");
+        double totalAmountOfMoney = account1.getAmount() + account2.getAmount() + account3.getAmount();
+
+        System.out.println(account1.createName("Alex") + "\n" + account1.getId() + "\n" + account1.getAmount() + "\n" + "__________________");
+        System.out.println(account2.createName("Liana") + "\n" + account2.getId() + "\n" + account2.getAmount() + "\n" + "__________________");
+        System.out.println(account3.createName("John") + "\n" + account3.getId() + "\n" + account3.getAmount() + "\n" + "__________________");
+        System.out.println("TOTAL BANK IS :  " + totalAmountOfMoney + " $"
+                + "\n" + "_______________________________________________________");
+
+        account1.setId(12l);
+        account1.setAmount(4000d);
+
+        account2.setAmount(1200d);
+
+
+        System.out.println("After recalculation:"
+                + "\n" + "********************"
+                + "\n" + "*************"
+                + "\n" + "*****");
+
+        System.out.println(account1.createName("Alex") + "\n" + account1.getId() + "\n" + account1.getAmount() + "\n" + "__________________");
+        System.out.println(account2.createName("Liana") + "\n" + account2.getId() + "\n" + account2.getAmount() + "\n" + "__________________");
+        System.out.println(account3.createName("John") + "\n" + account3.getId() + "\n" + account3.getAmount() + "\n" + "__________________");
+        totalAmountOfMoney = account1.getAmount() + account2.getAmount() + account3.getAmount();
+        System.out.println("TOTAL BANK IS :  " + totalAmountOfMoney + " $"
+                + "\n" + "_______________________________________________________");
     }
 }
