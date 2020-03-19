0d0e89d32e714dd6c3976905a1f3a0aafc08eb9e
naglec13 <naglec13@gmail.com>
2020-03-19 21:59:14 +0300

    ..

---
+++ src/ThirdChapter/examples/MusicStyle.java
@@ -0,0 +1,36 @@
+package ThirdChapter.examples;
+
+import java.util.Scanner;
+
+public enum MusicStyle {
+    JAZZ, ROCK, DRUM, TRANCE
+}
+
+class MusicRunner {
+    public static void main(String[] args) {
+
+        MusicStyle ms = MusicStyle.ROCK;
+        System.out.println(ms);
+        Scanner scanner = new Scanner(System.in);
+        String inputFromConsole = scanner.nextLine();
+        if (!inputFromConsole.equals(ms.name())){
+            System.out.println(inputFromConsole);
+        }
+        switch (ms){
+            case DRUM:
+                System.out.println("This this DRUM, BABY!");
+                break;
+            case JAZZ:
+                System.out.println("This this JAZZ, BABY!");
+                break;
+            case ROCK:
+                System.out.println("This this ROCK, BABY!");
+                break;
+            case TRANCE:
+                System.out.println("This this TRANCE, BABY!");
+                break;
+            default:
+                System.out.println("Couldn't find your genre..sorry");
+        }
+    }
+}
